2f75f1dfd5e4567d1502870cb51fd4e936edece5
Jan-Christoph Ihrens <ihrens@cc-services.de>
2018-02-15 11:35:31 +0100

    Small correction in the English language file

--- core/components/langrouter/lexicon/en/default.inc.php
+++ core/components/langrouter/lexicon/en/default.inc.php
@@ -1,6 +1,6 @@
 <?php
 $_lang['setting_langrouter.debug'] = 'Debug';
-$_lang['setting_langrouter.debug_desc'] = 'Log debug informations in MODX error log.';
+$_lang['setting_langrouter.debug_desc'] = 'Log debug information in MODX error log.';
 $_lang['setting_langrouter.response_code'] = 'Response Code';
 $_lang['setting_langrouter.response_code_desc'] = 'Response code for the redirect to the right context, if the culture key is not set.';
 $_lang['setting_langrouter.contextKeys'] = 'Context Keys';
