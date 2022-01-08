35c021cc4897bd08e782aecdf88fa27150de6142
Thomas Jakobi <thomas.jakobi@partout.info>
2022-01-08 10:57:02 +0100

    Update dependency docs

--- docs/index.md
+++ docs/index.md
@@ -6,12 +6,12 @@ depending on the URL requested by the client.
 
 ### Requirements
 
-- MODX Revolution 2.6+
-- PHP v5.6+
+* MODX Revolution 2.6+
+* PHP 5.6+
 
 ### Features
 
-- LangRouter works out-of-the-box and doesn't require any URL rewrite rule changes in the webserver configuration.
-- It checks the client's browser accepted languages and switches to the first accepted and available language context.
-- All routing is handled internally by MODX. This greatly simplifies the setup and provides portability. 
-- LangRouter was tested with Apache and Lighttpd.
+* LangRouter works out-of-the-box and doesn't require any URL rewrite rule changes in the webserver configuration.
+* It checks the client's browser accepted languages and switches to the first accepted and available language context.
+* All routing is handled internally by MODX. This greatly simplifies the setup and provides portability. 
+* LangRouter was tested with Apache and Lighttpd.
--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,25 @@ const gulp = require('gulp'),
     replace = require('gulp-replace'),
     pkg = require('./_build/config.json');
 
+let phpversion;
+let modxversion;
+pkg.dependencies.forEach(function (dependency, index) {
+    switch (pkg.dependencies[index].name) {
+        case 'php':
+            phpversion = pkg.dependencies[index].version.replace(/>=/, '');
+            break;
+        case 'modx':
+            modxversion = pkg.dependencies[index].version.replace(/>=/, '');
+            break;
+    }
+});
+
 gulp.task('bump-copyright', function () {
     return gulp.src([
         'core/components/langrouter/model/langrouter/langrouter.class.php',
         'core/components/langrouter/src/LangRouter.php',
     ], {base: './'})
-        .pipe(replace(/Copyright 2015(-\d{4})? by/g, 'Copyright ' + (new Date().getFullYear() > 2015 ? '2015-' : '') + new Date().getFullYear() + ' by'))
+        .pipe(replace(/Copyright 2015(-\d{4})? by/g, 'Copyright ' + (year > 2015 ? '2015-' : '') + year + ' by'))
         .pipe(gulp.dest('.'));
 });
 gulp.task('bump-version', function () {
@@ -21,10 +34,18 @@ gulp.task('bump-docs', function () {
     return gulp.src([
         'mkdocs.yml',
     ], {base: './'})
-        .pipe(replace(/&copy; 2015(-\d{4})?/g, '&copy; ' + (new Date().getFullYear() > 2015 ? '2015-' : '') + new Date().getFullYear()))
+        .pipe(replace(/&copy; 2015(-\d{4})?/g, '&copy; ' + (year > 2015 ? '2015-' : '') + year))
         .pipe(gulp.dest('.'));
 });
-gulp.task('bump', gulp.series('bump-copyright', 'bump-version', 'bump-docs'));
+gulp.task('bump-requirements', function () {
+    return gulp.src([
+        'docs/index.md',
+    ], {base: './'})
+        .pipe(replace(/[*-] MODX Revolution \d.\d.*/g, '* MODX Revolution ' + modxversion + '+'))
+        .pipe(replace(/[*-] PHP (v)?\d.\d.*/g, '* PHP ' + phpversion + '+'))
+        .pipe(gulp.dest('.'));
+});
+gulp.task('bump', gulp.series('bump-copyright', 'bump-version', 'bump-docs', 'bump-requirements'));
 
 // Default Task
 gulp.task('default', gulp.series('bump'));
