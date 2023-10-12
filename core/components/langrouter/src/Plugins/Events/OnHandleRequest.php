ec3756638e1eb8a2ee7bf0e861620643f6a37d9e
Jens Wittmann – Gestaltung & Entwicklung <jenswittmann@users.noreply.github.com>
2023-10-12 20:18:30 +0200

    Update OnHandleRequest.php

--- core/components/langrouter/src/Plugins/Events/OnHandleRequest.php
+++ core/components/langrouter/src/Plugins/Events/OnHandleRequest.php
@@ -84,7 +84,7 @@ class OnHandleRequest extends Plugin
                 // Set locale since $this->modx->_initCulture is called before OnHandleRequest
                 if ($this->modx->context && $this->modx->getOption('setlocale', null, true)) {
                     $locale = setlocale(LC_ALL, 0);
-                    setlocale(LC_ALL, $this->modx->context->getOption('locale', 0, $locale));
+                    setlocale(LC_ALL, $this->modx->context->getOption('locale', null, $locale));
                 }
             }
         }
